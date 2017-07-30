d86da9de534082e7ea560a7fdb7e7db6eac24247
Yusank <yusankurban@gmail.com>
2017-07-30 13:04:41 +0800

    fix panic problem

--- wxweb/session.go
+++ wxweb/session.go
@@ -307,7 +307,8 @@ func (s *Session) analize(msg map[string]interface{}) *ReceivedMessage {
 		OriginContent: msg["Content"].(string),
 		FromUserName:  msg["FromUserName"].(string),
 		ToUserName:    msg["ToUserName"].(string),
-		MsgType:       int(msg["SubMsgType"].(float64)),
+		MsgType:       int(msg["MsgType"].(float64)),
+		SubType: 	   int(msg["SubMsgType"].(float64)),
 		Url: 		   msg["Url"].(string),
 	}
 
