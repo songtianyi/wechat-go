6cb7e47e43de23576f5a24f2feb6b531d5826d88
owen.gao <owen.gao@analyticservice.net>
2018-05-16 14:52:47 +0800

    update img send error

--- wxweb/apiv2.go
+++ wxweb/apiv2.go
@@ -456,9 +456,7 @@ func (api *ApiV2) WebWxUploadMedia(common *Common, ce *XmlConfig, cookies []*htt
 	jar.SetCookies(u, cookies)
 	api.httpClient.SetJar(jar)
 
-	headers := make(map[string]string)
-	headers["Content-Type"] = w.FormDataContentType()
-	body, _ := api.httpClient.fetchWithReader("POST", common.UploadUrl, &b, headers)
+	body, _ := api.httpClient.fetchWithReader("POST", common.UploadUrl, &b, Header{"Content-Type": w.FormDataContentType()})
 
 	jc, err := rrconfig.LoadJsonConfigFromBytes(body)
 	if err != nil {
